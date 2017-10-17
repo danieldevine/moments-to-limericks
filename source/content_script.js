bb99d47cb540bbb4d4a8fe3414cf18569a778add
Dan Devine <dan@maverick-intl.com>
2017-10-17 15:48:54 +0100

    make this actully work ffs

--- source/content_script.js
+++ source/content_script.js
@@ -1,7 +1,7 @@
-$('window').on('load', function(){
-   var moments = $('.moments');
-   var link = moments.find('a');
-   var limerick = moments.find('span.text');
-   link.attr('href', 'https://haiku.hell-island.com/limrixx');
-   limerick.text('Limrixx');
-});
+var moments = $('.moments');
+var link = moments.find('a');
+var limerick = moments.find('span.text');
+
+link.attr('href', 'https://haiku.coderjerk.com/limrixx.php');
+limerick.text('Limrixx');
+
