ead8ab244f85bc4c8f50449d7d599284f99efa44
Dan Devine <dan@maverick-intl.com>
2017-10-17 15:08:46 +0100

    initializes the crappile

---
+++ README.md
@@ -0,0 +1,7 @@
+#  Moments to Limericks
+
+Chrome extension that replaces the link to Twitter Moments with one to Cool Bot generated Limericks.
+
+## Installation
+
+Now available on the Chrome Store! Search for Moments to Limericks
---
+++ source/content_script.js
@@ -0,0 +1,7 @@
+$('window').on('load', function(){
+   var moments = $('.moments');
+   var link = moments.find('a');
+   var terxt = moments.find('span.text');
+   link.attr('href', 'https://haiku/hell-island.com/limrixx');
+   terxt.text('Limrixx')
+});
